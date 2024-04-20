c77b4b6e4d47c95ca06baf56ceb4823a29941da6
TheSilentSage <harigovindyt214@gmail.com>
2024-04-20 21:49:47 +0530

    feat: animation on scroll

--- Main/script.js
+++ Main/script.js
@@ -1,3 +1,6 @@
+window.addEventListener('scroll',reveal)
+
+
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
@@ -7,3 +10,26 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+function reveal(){
+    var reveals = document.querySelectorAll("section")
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var revealTop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 250;
+        var unrevealPoint = revealpoint + 950;
+
+        if (revealTop < windowHeight - revealpoint){
+            reveals[i].classList.add('active');
+        }
+        else{
+            reveals[i].classList.remove('active');
+        }
+        if (revealTop < windowHeight - unrevealPoint){
+            reveals[i].classList.add('inactive');
+        }else{
+            reveals[i].classList.remove('inactive');
+        }
+        
+    }
+}
